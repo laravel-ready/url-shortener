c6f9d3d1bbc2246e0d2b6af4e54c54e38d1e26dc
Egoist <epsilonelittr@gmail.com>
2023-03-17 07:02:17 +0300

    Add eloquent support

---
+++ src/Supports/Eloquent.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace LaravelReady\UrlShortener\Supports;
+
+use Illuminate\Container\Container;
+use Illuminate\Database\Connection;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\ConnectionResolverInterface;
+use Illuminate\Database\Connectors\ConnectionFactory;
+
+class Eloquent {
+    /**
+     * Init the database connection
+     */
+    public static function initDbConnection(): void
+    {
+        // Create a new container instance
+        $container = new Container();
+
+        // Create a new connection factory and pass in the container
+        $factory = new ConnectionFactory($container);
+
+        // Define your database configuration options
+        $config = [
+            'driver' => 'sqlite',
+            'database' => database_path('migrations/laravel-ready/url-shortener/' . Config::get('url-shortener.emoji.database', 'emojipadia-v1.0')),
+            'prefix' => '',
+            'foreign_key_constraints' => true,
+        ];
+
+        // Use the factory to create a new connection
+        $connection = $factory->make($config);
+
+        // Set the connection resolver on the Eloquent ORM
+        Model::setConnectionResolver(new class($connection) implements ConnectionResolverInterface {
+            private $connection;
+            
+            public function __construct(Connection $connection) {
+                $this->connection = $connection;
+            }
+
+            public function connection($name = null) {
+                return $this->connection;
+            }
+
+        	/**
+             * Get the default connection name.
+             * @return string
+             */
+            public function getDefaultConnection() {}
+            
+            /**
+             * Set the default connection name.
+             *
+             * @param string $name
+             * @return void
+             */
+            public function setDefaultConnection($name) {}
+        });
+    }
+}

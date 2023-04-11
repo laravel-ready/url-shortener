db248234b76cd35113ac0bc56f483b04eee24141
Egoist <epsilonelittr@gmail.com>
2023-04-11 13:03:14 +0300

    Load emojis from cache

--- src/Http/Controllers/EmojiController.php
+++ src/Http/Controllers/EmojiController.php
@@ -3,6 +3,8 @@
 namespace LaravelReady\UrlShortener\Http\Controllers;
 
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Config;
 use LaravelReady\UrlShortener\Supports\Emoji;
 use LaravelReady\UrlShortener\Supports\Eloquent;
 
@@ -10,11 +12,21 @@ class EmojiController extends Controller
 {
     public function __invoke()
     {
+        $emojisQuery = Emoji::getBaseEmojiQuery();
+
+        $cacheKey = Config::get('url-shortener.table_name', 'short_url') . '_emojis.' . md5($emojisQuery->toSql());
+
+        if ($cacheKey) {
+            return response()->json($cacheKey);
+        }
+
         Eloquent::initNewDbConnection();
 
-        $emojis = Emoji::getBaseEmojiQuery()->get();
+        $emojis = $emojisQuery->get();
 
         Eloquent::restorePreviousDbConnection();
+
+        Cache::put($cacheKey, $emojis, now()->addYear());
 
         return response()->json($emojis);
     }

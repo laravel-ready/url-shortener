1678e79729678b7fecc91b93272ef707cd85cb9f
Egoist <epsilonelittr@gmail.com>
2023-05-07 22:11:13 +0300

    Update length limits

--- src/Http/Requests/CreateShortUrlRequest.php
+++ src/Http/Requests/CreateShortUrlRequest.php
@@ -21,7 +21,7 @@ class CreateShortUrlRequest extends FormRequest
         return [
             'url' => 'required|url',
             'type' => 'required|in:random,custom,emoji,emoji_custom',
-            'short_code' => 'required_if:type,custom,emoji_custom|nullable|string|min:3|max:100',
+            'short_code' => 'required_if:type,custom,emoji_custom|nullable|string|min:1|max:50',
             'meta.title' => 'nullable|string|min:3|max:200',
             'meta.description' => 'nullable|string|min:3|max:1000',
             'meta.status' => 'nullable|boolean',

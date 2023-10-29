6d85c1e5d3dfa526c8d621d2f92d0e49136ecdb7
Eyüp Erdoğan <epsilonelittr@gmail.com>
2023-10-29 16:50:04 +0300

    Update unicode emoji model

--- src/Models/Emoji/UnicodeEmoji.php
+++ src/Models/Emoji/UnicodeEmoji.php
@@ -3,16 +3,39 @@
 namespace LaravelReady\UrlShortener\Models\Emoji;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class UnicodeEmoji extends Model
-{    protected $table = 'Unicode_Emoji';
+{
+    public $fillable = [
+        'emoji',
+        'name',
+        'description',
+        'codePoint',
+        'shortCode',
+        'isLayered',
+        'hasZeroWidthSpace',
+        'testedChromiumVersion',
+        'slug',
+        'keywords',
+        'groupName',
+        'isActive',
+    ];
 
-    public function version()
+    public $timestamps = false;
+
+    protected $table = 'Unicode_Emoji';
+
+    protected $casts = [
+        'isActive' => 'boolean',
+    ];
+
+    public function version(): BelongsTo
     {
         return $this->belongsTo(UnicodeVersion::class, 'unicode_VersionId');
     }
 
-    public function emojiVersion()
+    public function emojiVersion(): BelongsTo
     {
         return $this->belongsTo(UnicodeEmojiVersion::class, 'unicode_Emoji_VersionId');
     }
